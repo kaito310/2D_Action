fd80c7a07831ef1bbbe55f19340a55dcbd1df77b
Nishi.F <SDT2395GP0016@edu.sca.ac.jp>
2023-07-20 16:30:57 +0900

    Playerにジャンプを追加

--- Assets/Nishi/Script/PlayerController.cs
+++ Assets/Nishi/Script/PlayerController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerController: MonoBehaviour
 {
     [SerializeField] float speed = 1;
+    [SerializeField] float jump = 1;
+    private Rigidbody2D _rd2D;
+    private int jumpCount = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rd2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,16 +23,32 @@ public class PlayerController: MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
            position.x -= speed * Time.deltaTime;
+           if (Input.GetKey(KeyCode.D))
+            {
+                position.x += speed * Time.deltaTime;
+            }
         }
         if (Input.GetKey(KeyCode.D))
         {
             position.x += speed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.A))
+            {
+                position.x -= speed * Time.deltaTime;
+            }
         }
         if (Input.GetKey(KeyCode.Space))
         {
-
+            _rd2D.AddForce(transform.up * jump);
+            jumpCount++;
         }
 
         transform.position = position;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("floor"))
+        {
+            jumpCount = 0;
+        }
     }
 }

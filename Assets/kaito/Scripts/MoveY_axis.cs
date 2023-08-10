72b109f7d1a198c110b30a6ca7742dca0c4eeae3
kaito310 <7415kaito.s@gmail.com>
2023-08-10 15:08:28 +0900

    足場の動き調整（完了）

--- Assets/kaito/Scripts/MoveY_axis.cs
+++ Assets/kaito/Scripts/MoveY_axis.cs
@@ -2,19 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//----足場を上下に動かすスクリプト----
+//----オブジェクトを上下に動かすスクリプト----
 public class MoveY_axis : MonoBehaviour
 {
-    [SerializeField] float _length; // 往復範囲
-    float moveY; // y軸方向の値
+    [SerializeField] float _speed; // 移動速度
+    [SerializeField] int _num = 1; // 左右移動切り替え用の変数
+    [SerializeField] float _yBounds; // y軸方向の移動の範囲
+
+    Vector2 startPos; // 初期位置
+    Vector2 pos; // 現在位置
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // 往復した値を時間から計算
-        moveY = Mathf.PingPong(Time.time, _length);
+        pos = transform.position;
 
-        // y座標を往復させて上下に動かす
-        transform.localPosition = new Vector2(transform.position.x, moveY);
+        // 現在位置が初期位置＋移動範囲より大きくなったら
+        if (pos.y > startPos.y + _yBounds)
+        {
+            _num = -1; // 下に移動させるように
+        }
+        if (pos.y < (startPos.y - _yBounds))
+        {
+            _num = 1; // 上に移動させるように
+        }
+
+        // _numがマイナスになると逆方向に移動する
+        transform.Translate(transform.up * Time.deltaTime * _speed * _num);
     }
 }

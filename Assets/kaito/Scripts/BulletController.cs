6c4b8599fb9028738d52229b6d2df18374f28c7f
kaito310 <7415kaito.s@gmail.com>
2023-08-22 14:11:18 +0900

    弾を発射する敵のスクリプト作成途中

--- Assets/kaito/Scripts/BulletController.cs
+++ Assets/kaito/Scripts/BulletController.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //----敵から発射される弾のスクリプト----
+/* ・弾の現在位置（移動中の位置）に生成されてしまう
+   ・破壊できていない */
 public class BulletController : MonoBehaviour
 {
-    [SerializeField] Vector3 _velocity; // 速度
+    [SerializeField] float _velocity; // 速度
+    [SerializeField] GameObject parentEnemy;
+    [SerializeField] GameObject bulletPrefab;
+    
+    float elapsedTime; // 経過時間
+    [SerializeField] float timeBounds; // 時間上限
+    [SerializeField] float deleteTime; // 破壊までの時間
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +24,26 @@ public class BulletController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector2(_velocity, 0f) * Time.deltaTime);
+        Shooting();
+    }
+
+    void Shooting() // 敵が弾を発射する関数
+    {
+        elapsedTime += Time.deltaTime; // 毎Fの時間を足していく
+
+        // 親オブジェクトが存在するとき、かつ経過時間が時間上限（秒）を越えたら
+        if (parentEnemy && elapsedTime > timeBounds)
+        {
+            Instantiate(bulletPrefab, transform.position, transform.rotation); // 弾を生成
+
+            elapsedTime = 0; // 経過時間リセット
+        }
+
+        if (elapsedTime == deleteTime) // 経過時間が破壊時間を越えたら
+        {
+            Debug.Log("a");
+            Destroy(gameObject);
+        }
     }
 }

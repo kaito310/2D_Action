4760463f336e0acc65fac1b3162651ff301bd775
kaito310 <7415kaito.s@gmail.com>
2023-09-08 16:29:07 +0900

    ステージにUI追加途中

---
+++ Assets/kaito/Scripts/UIManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//---UIを管理するためのスクリプト---
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject _gameClearUI;
+    [SerializeField] GameObject _gameOverUI;
+    [SerializeField] Button _retryButton;
+
+    bool _isGameClear = false; // ゲームクリアしてるか判別のため
+    bool _isGameOver = false;
+
+    PlayerController _playerControllerScript; // プレイヤーキャラのスクリプト
+
+    void Start()
+    {
+        _playerControllerScript = GameObject.Find("Player").
+            GetComponent<PlayerController>();
+        _gameClearUI.SetActive(false);
+        _gameOverUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        // ゲームクリアの処理
+        if (!_isGameClear)
+        {
+            /*if (_playerControllerScript.isClear) // PlayerControllerにisClearを追加予定
+            {
+                _gameClearUI.SetActive(true);
+                _isGameClear = true;
+            }*/
+        }
+
+        // ゲームオーバーの処理
+        if (!_isGameOver)
+        {
+            if (_playerControllerScript.isDead)
+            {
+                _gameOverUI.SetActive(true);
+                _isGameOver = true;
+            }
+        }
+    }
+
+    public void RetryButton() // リトライボタンが押されたら
+    {
+        SceneManager.LoadScene("MainGame"); // ゲームの再読み込み
+    }
+}
